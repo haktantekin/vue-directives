c9faafaa3c7bb554e6abfd30562bff17615a938f
haktantekin <47484364+haktantekin@users.noreply.github.com>
2021-10-09 13:58:14 +0300

    Multi modifier

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,13 @@
     <div class="row">
       <div class="col-md-6 col-md-offset-3">
         <h3>Custom Directive</h3>
-        <p v-color:background.delay="'green'">
+        <p v-color:background.delay.flash="'green'">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
           aspernatur beatae culpa doloribus, ex facilis fugiat, fugit magni
           molestiae mollitia, nesciunt nihil perspiciatis quaerat quam quas
           ratione sequi unde ut!
         </p>
-        <p v-color.delay="'red'">
+        <p v-color.delay.flash="'red'">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
           aspernatur beatae culpa doloribus, ex facilis fugiat, fugit magni
           molestiae mollitia, nesciunt nihil perspiciatis quaerat quam quas
@@ -35,21 +35,42 @@ export default {
   directives: {
     color: {
       bind(el, binding, vnode) {
+
+        let delay = 0;
+
         if (binding.modifiers["delay"]) {
-          setTimeout(() => {
-            if (binding.arg == "background") {
-              el.style.backgroundColor = binding.value;
-            } else {
-              el.style.color = binding.value;
-            }
-          }, 2000);
-        } else {
-          if (binding.arg == "background") {
-            el.style.backgroundColor = binding.value;
-          } else {
-            el.style.color = binding.value;
-          }
+
+          delay = 2000;
+        
+        } 
+
+        if(binding.modifiers["flash"]){
+          let firstColor = binding.value;
+          let secondColor = '#fbbd08';
+          let currentColor = firstColor;
+             setTimeout(() => {
+               setInterval(()=>{
+                currentColor == secondColor ? currentColor = firstColor : currentColor = secondColor;
+                
+                if (binding.arg == "background") {
+                  el.style.backgroundColor = currentColor;
+              } else {
+                el.style.color = currentColor;
+              }
+               }, 1000)
+            }, delay);
+
         }
+        else{
+          setTimeout(() => {
+              if (binding.arg == "background") {
+                el.style.backgroundColor = binding.value;
+              } else {
+                el.style.color = binding.value;
+              }
+            }, delay);
+        }
+
       }
     }
   }
