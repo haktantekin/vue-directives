3c19e42eb5bfa64a37437706176d2ff366864415
haktantekin <47484364+haktantekin@users.noreply.github.com>
2021-10-09 13:47:39 +0300

    Local and global directive describing

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+        <h3>VueJS ile gelen Directive Tanımları</h3>
+        <p v-text="'Deneme yanilma metodu her zaman iyidir..'"></p>
+        <p v-html="'<strong>Bu da v-html</strong>'"></p>
+      </div>
+    </div>
+    <hr />
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+        <h3>Custom Directive</h3>
+        <p v-color:background.delay="'green'">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
+          aspernatur beatae culpa doloribus, ex facilis fugiat, fugit magni
+          molestiae mollitia, nesciunt nihil perspiciatis quaerat quam quas
+          ratione sequi unde ut!
+        </p>
+        <p v-color.delay="'red'">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
+          aspernatur beatae culpa doloribus, ex facilis fugiat, fugit magni
+          molestiae mollitia, nesciunt nihil perspiciatis quaerat quam quas
+          ratione sequi unde ut!
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// Local directive tanımı
+
+export default {
+  directives: {
+    color: {
+      bind(el, binding, vnode) {
+        if (binding.modifiers["delay"]) {
+          setTimeout(() => {
+            if (binding.arg == "background") {
+              el.style.backgroundColor = binding.value;
+            } else {
+              el.style.color = binding.value;
+            }
+          }, 2000);
+        } else {
+          if (binding.arg == "background") {
+            el.style.backgroundColor = binding.value;
+          } else {
+            el.style.color = binding.value;
+          }
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
